58d67856da025c84b89deb4ca4478710d06ccc65
Danish <danishshamshee123@gmail.com>
2025-05-27 10:40:15 +0530

    changein on isLoddedIn middleware

--- Backend/middleware/isLoggedin.js
+++ Backend/middleware/isLoggedin.js
@@ -8,7 +8,8 @@ module.exports = async(req, res, next)=>{
     if(!req.cookies.token) return res.status(401).json({message: "cookie not found"});
 
     try {
-        let decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET_KEY);
+        // let decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET_KEY);
+        let decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET_KEY);
         let user = await userModel.findOne({_id: decoded.id}).select('-password');
         req.user = user;
         next();
