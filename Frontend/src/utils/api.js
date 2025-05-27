462205e2f140bb4a349e0792793367d0decc17ff
Danish <danishshamshee123@gmail.com>
2025-05-27 10:53:43 +0530

    changein on isLoddedIn middleware

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -15,7 +15,7 @@ api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   console.log(`Frontend token: ${token}`);
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = token;
   }
     return config;
 }, (error) => {
